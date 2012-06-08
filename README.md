59c9f278a9e0c52e4c57aecb48ddb1a80abf7c1e
David Mertl <dave@onzra.com>
2012-06-08 15:50:12 -0700

    Adding copyright notices

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ CakeRateLimiter
 ===============
 
 A throttling rate limiter for use with CakePHP
+
+Licensing
+===============
+
+ONZRA: Enterprise Development
+Copyright 2012, ONZRA LLC (http://www.ONZRA.com)
+
+Licensed under The MIT License
+Redistributions of files must retain the above copyright notice.
--- Test/Case/Lib/RateLimitTest.php
+++ Test/Case/Lib/RateLimitTest.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * RateLimiting support for CakePHP
+ *
+ * ONZRA: Enterprise Development
+ * Copyright 2012, ONZRA LLC (http://www.ONZRA.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2012, ONZRA LLC (http://www.ONZRA.com)
+ * @link          https://github.com/onzra/CakeRateLimiter CakeRateLimiter
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 App::uses('RateLimit', 'RateLimiter.Lib');
 
